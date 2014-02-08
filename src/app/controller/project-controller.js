a2be3d1f9ec8cb76f52db76cdbe126d93be2f3fb
Damien Klinnert <mail@damienklinnert.de>
2014-02-08 18:40:51 +0100

    add todo comments

--- src/app/controller/project-controller.js
+++ src/app/controller/project-controller.js
@@ -4,7 +4,7 @@
     exports.angular.module('app.projectController', [])
         .controller('projectController', ProjectController);
 
-    function ProjectController($scope, project) {
+    function ProjectController($scope, project, store) {
         $scope.project = project;
 
         $scope.removeInputGlob = function (task, index) {
@@ -20,6 +20,15 @@
             // todo
             console.log('add step at ' + position);
         };
+
+        // debounce?
+        var unregisterProjectWatch = $scope.$watch('project', function () {
+            store.saveProject($scope.project);
+        }, true);
+
+        $scope.$on('$destroy', function () {
+            unregisterProjectWatch();
+        });
     }
 
 }(this));
--- src/store/store.js
+++ src/store/store.js
@@ -4,17 +4,18 @@
     var PROJECT_LIST_KEY = 'projects', PROJECT_PREFIX = 'PR_';
     // @TODO: localStorage other tab?
     // @TODO: localStorage size exceeded?
+    // @TODO: localStorage add try catch
+    // @TODO: also use dropbox / use promises?
     exports.angular.module('store', ['localStorage'])
         .factory('store', function (localStorage) {
             return {
                 loadProjects: function () {
                     var projectNames = localStorage.get(PROJECT_LIST_KEY);
-
+                    // if projects are corrupted, reset localStorage
                     if (!projectNames || !projectNames.length) {
                         localStorage.reset();
                         return [];
                     }
-
                     return projectNames.map(function (projectName) {
                         return localStorage.get(PROJECT_PREFIX + projectName);
                     }).filter(function (res) { return res; });
@@ -23,9 +24,7 @@
                     var projectNames = projects.map(function (project) {
                         return normalizeName(project.name);
                     });
-
                     localStorage.set(PROJECT_LIST_KEY, projectNames);
-
                     projects.forEach(this.saveProject);
                 },
                 saveProject: function (project) {
