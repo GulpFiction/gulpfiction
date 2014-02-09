0f85aff3dcc07d731dc441d70e87049036f25525
Damien Klinnert <mail@damienklinnert.de>
2014-02-09 03:23:51 +0100

    make db work (needs refactoring, so ;-))

--- src/app/controller/app-controller.js
+++ src/app/controller/app-controller.js
@@ -1,13 +1,16 @@
 (function (exports) {
     'use strict';
 
+    var DROPBOX_KEY = 'dropbox_credentials';
+
     exports.angular.module('app.appController', [
         'gulp.gulp',
         'builder.fileBuilder',
-        'ngAnimate'
+        'ngAnimate',
+        'localStorage'
     ]).controller('appController', AppController);
 
-    function AppController($scope, gulp, fileBuilder, $location, $rootScope, Dropbox) {
+    function AppController($scope, gulp, fileBuilder, $location, $rootScope, Dropbox, localStorage) {
 
         $scope.projects = gulp.listProjects();
 
@@ -48,24 +51,61 @@
 
         $scope.connectDb = function () {
             window.Dropbox = Dropbox;
+            var dropboxCredentials = localStorage.get(DROPBOX_KEY);
 
-            Dropbox.authenticate().then(function (response) {
-                // @TODO: store response.access_token in localstorage
-
-                Dropbox.accountInfo().then(function (data) {
-                    $scope.dbAccountInfo = data;
+            if (!dropboxCredentials) {
+                Dropbox.authenticate().then(function (response) {
+                    // @TODO: store response.access_token in localstorage
+                    
+                    $scope.isDropboxAuthenticated = true;
+                    Dropbox.accountInfo().then(function (data) {
+                        console.log(data);
+                        $scope.dbAccountInfo = data;
+                        localStorage.set(DROPBOX_KEY, { token:response.access_token, uid: data.uid });
+                    });
+                });   
+            } else {
+                Dropbox.setCredentials({
+                    access_token: dropboxCredentials.token,
+                    token_type: "bearer",
+                    uid: dropboxCredentials.uid
                 });
-            });
+                
+                Dropbox.accountInfo().then(function (data) {
+                    $scope.isDropboxAuthenticated = true;
+                    $scope.dbAccountInfo = data;
+                }, function(error) {
+                    Dropbox.authenticate().then(function (response) {
+                        // @TODO: store response.access_token in localstorage
+                        console.log(response.access_token);
+                        localStorage.set(DROPBOX_KEY, response.access_token);
+                        $scope.isDropboxAuthenticated = true;
+                        Dropbox.accountInfo().then(function (data) {
+                            $scope.dbAccountInfo = data;
+                            localStorage.set(DROPBOX_KEY, { token:response.access_token, uid: data.uid });
+                        });
+                    });   
+                });
+            }
+            
         };
 
-        $scope.exportProject = function (project) {
+
+
+
+        $scope.exportProjectToDropbox = function (project) {
             if (Dropbox.isAuthenticated()) {
                 console.log('write to DB');
                 var file = fileBuilder.build(project);
                 Dropbox.writeFile(project.name + '.js', file, {mime_type: 'application/javascript'});
             }
+        };
+
+        $scope.exportProject = function (project) {
             $scope.currentExportContent = fileBuilder.build(project);
             $scope.showsExport = true;
+            var token = localStorage.get(DROPBOX_KEY) || '';
+            // Dropbox.options.headers.Authorization = 'Bearer ' + token;
         };
     }
 
